34ae2f5244e619f3b166dbf5fff098e9cc234d3b
ricolangue <ricolangue@gmail.com>
2024-10-26 08:08:54 +0800

    t34234 - BU

---
+++ db/wordpress_password.txt.sql
@@ -0,0 +1,10 @@
+$P$BuBy6GZ4ATLOeNpWHmT7.OKomb/GxB.
+password: admin74752
+
+$P$BXlvrTMa8gEPgwFI/nbmhQ/7C6JciW.
+password: admin
+
+
+Gmail NEW Login details
+Email: webtest2g@gmail.com
+Password: bAB7i/6T&i(7
